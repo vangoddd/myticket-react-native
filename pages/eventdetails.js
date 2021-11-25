d4a28eb5df18816a9892e5945d2b61c08466fddb
Vangod <vachri.attala@gmail.com>
2021-11-25 21:34:37 +0700

    styling event details page

--- pages/eventdetails.js
+++ pages/eventdetails.js
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, Image, ScrollView} from 'react-native';
+import 'intl';
 
 import storage from '@react-native-firebase/storage';
+
+const convertToRupiah = price => {
+  // return new Intl.NumberFormat('en', {
+  //   style: 'currency',
+  //   currency: 'IDR',
+  // }).format(price);
+  return price;
+};
 
 export default function EventDetails({route, navigation}) {
   const item = route.params;
@@ -9,44 +18,37 @@ export default function EventDetails({route, navigation}) {
   return (
     <ScrollView>
       <View style={styles.itemStyle}>
-        <Text style={styles.header}>{item.name}</Text>
-        <Image
-          style={{
-            width: '100%',
-            height: 200,
-            borderRadius: 20,
-            marginVertical: 10,
-          }}
-          source={{uri: item.image}}
-        />
-        <Text style={styles.textStyle}>{item.description}</Text>
-        <Text style={styles.textStyle}>Lokasi : {item.location}</Text>
-        <Text style={styles.textStyle}>
-          Tiket Tersedia : {item.available_ticket}
-        </Text>
-        <Text style={styles.textStyle}>Harga {item.price}</Text>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>{item.name}</Text>
+        </View>
+
+        <Image style={styles.imageStyle} source={{uri: item.image}} />
+
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardTitle}>Description</Text>
+
+          <Text style={styles.textStyle}>{item.description}</Text>
+        </View>
+
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardTitle}>Location</Text>
+          <Text style={styles.textStyle}>{item.location}</Text>
+        </View>
+
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardTitle}>Available Ticket</Text>
+          <Text style={styles.textStyle}>{item.available_ticket}</Text>
+        </View>
+        <View style={styles.cardContainer}>
+          <Text style={styles.cardTitle}>Price</Text>
+          <Text style={styles.textStyle}>{convertToRupiah(item.price)}</Text>
+        </View>
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
   textStyle: {
     fontSize: 15,
     padding: 5,
@@ -54,10 +56,34 @@ const styles = StyleSheet.create({
   itemStyle: {
     flex: 1,
     alignItems: 'flex-start',
-    margin: 20,
   },
   header: {
-    fontSize: 25,
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 30,
+    margin: 20,
+    color: '#444',
+    textAlign: 'center',
+  },
+  imageStyle: {
+    width: '100%',
+    height: 200,
+    borderRadius: 0,
     marginBottom: 10,
+  },
+  cardContainer: {
+    width: '100%',
+    backgroundColor: 'white',
+    marginBottom: 10,
+    padding: 15,
+  },
+  cardTitle: {
+    color: '#444',
+    fontSize: 20,
+    fontFamily: 'Montserrat-Medium',
+    paddingHorizontal: 5,
+  },
+  headerContainer: {
+    backgroundColor: 'white',
+    width: '100%',
   },
 });
